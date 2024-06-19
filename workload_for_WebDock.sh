2dd5c4c84388a2b613b6c0c2d3ca58e9a97b5743
maxwellcrawford21 <maxwellcrawford.2@gmail.com>
2024-06-19 20:23:12 +0200

    Update workload_for_WebDock.sh

--- workload_for_WebDock.sh
+++ workload_for_WebDock.sh
@@ -9,7 +9,7 @@ currentdate=$(date '+%d-%b-%Y_WebDock_')
 ipaddress=$(curl -s ifconfig.me)
 underscored_ip=$(echo $ipaddress | sed 's/\./_/g')
 currentdate+=$underscored_ip
-used_num_of_cores=`expr $num_of_cores - 3`
+used_num_of_cores=`expr $num_of_cores - 6`
 
 echo ""
 echo "You have a total number of $used_num_of_cores cores"
